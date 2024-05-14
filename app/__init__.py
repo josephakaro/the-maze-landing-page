81f09b4dbba82160fd3c1d44b83149edab01b16e
Joseph Akaro <josephmmakaro@gmail.com>
2024-05-14 22:46:15 +0300

    Step by step installations

--- app/__init__.py
+++ app/__init__.py
@@ -1,26 +1,23 @@
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
+app = Flask(__name__)
 
-    from app.auth import login
-    from app.home import index
-    from app.routes import about
-    from app.routes import contact
-    from app.routes import testimonial
-    from app.routes import forum
-    from app.routes import support
-    from app.routes import game
-    from app.routes import faqs
+from app.auth import login
+from app.home import index
+from app.routes import about
+from app.routes import contact
+from app.routes import testimonial
+from app.routes import forum
+from app.routes import support
+from app.routes import game
+from app.routes import faqs
 
-    app.register_blueprint(login.login)
-    app.register_blueprint(index.index)
-    app.register_blueprint(about.about)
-    app.register_blueprint(contact.contact)
-    app.register_blueprint(testimonial.testimonial)
-    app.register_blueprint(forum.forum)
-    app.register_blueprint(support.support)
-    app.register_blueprint(game.game)
-    app.register_blueprint(faqs.faqs)
-
-    return app
+app.register_blueprint(login.login)
+app.register_blueprint(index.index)
+app.register_blueprint(about.about)
+app.register_blueprint(contact.contact)
+app.register_blueprint(testimonial.testimonial)
+app.register_blueprint(forum.forum)
+app.register_blueprint(support.support)
+app.register_blueprint(game.game)
+app.register_blueprint(faqs.faqs)
--- app/db/db.py
+++ app/db/db.py
@@ -1,7 +1,46 @@
-# import sqlalchemy
-# from flask_login import UserMixin
+import sqlalchemy
+from flask_login import UserMixin
+from app import app
+db = sqlalchemy(app)
 
-# class User(db.Model, UserMixin):
-#     id = db.Column(db.Integer, primary_key=True)
-#     username = db.Column(db.String(20), nullable=False)
-#     password = db.Column(db.String(80), nullable=False)
+# Users Schema
+class User(db.Model, UserMixin):
+    """
+        id: User Id, Integer Value
+        UserName: Username, String Value
+        Password: Password, Holds long Integer Values
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    Username = db.Column(db.String(20), nullable=False)
+    Password = db.Column(db.String(80), nullable=False)
+
+
+
+# Contact Us Schema
+class Contact(db.Model):
+    """
+        id: Contact ID, Integer Values
+        FirstName: First Name field, String Value
+        LastName: Last Name field, String Value
+        Email: Email Address field, Strinf Value
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    FirstName = db.Column(db.String(20), nullable=False)
+    LastName = db.Column(db.String(20), nullable=False)
+    Email = db.Column(db.String(80), nullable=False)
+
+# Blog Post Schema
+class Blog(db.Model):
+    """
+    id: Auto Generated Integer ID, Auto_Increment in MySQL
+    Title: String Values
+    Contents: Holds the main content of the blog post
+    PublishDate: Holds the Publish Date of the Blog post
+    ModifiedDate: Hold the Date the Blog post changed by the User
+    """
+    id = db.Column(db.Integer, primary_key=True)
+    Title = db.Column(db.String(100), nullable=False)
+    Contents = db.Column(db.String(250), nullable=False)
+    PublishDate = db.Column(db.Integer, nullable=False)
+    ModifiedDate = db.Column(db.Integer, nullable=False)
+    UserID = db.Column(db.Integer, foriegn_key=True)
